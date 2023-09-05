8c43406ecd17dda81ac38039c0a9f1c4bc3b6e4e
Alejandro Tardín <alejandro.tardin@liferay.com>
2023-09-05 11:23:29 +0200

    LPS-194865 Fixing multiCompany for already processed bundles
    When a bundle has been processed and the server is restarted the portal
    instance lifecycle listeners are not being registered.
    
    This means the batch engine units will not be processed for new
    companies created after that.
    
    With this change we ensure the units are registered even if the
    bundle has already been processed.
    
    We also go from N listeners where N is the number of bundles to just 1
    listener for all of them, which is quite good.

---
+++ modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/unit/MultiCompanyBatchEngineUnitProcessor.java
@@ -0,0 +1,95 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.batch.engine.internal.unit;
+
+import com.liferay.batch.engine.internal.bundle.CompanyBatchEngineUnitWrapper;
+import com.liferay.batch.engine.unit.BatchEngineUnit;
+import com.liferay.batch.engine.unit.BatchEngineUnitProcessor;
+import com.liferay.petra.function.transform.TransformUtil;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.osgi.framework.Bundle;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alejandro Tardín
+ */
+@Component(service = MultiCompanyBatchEngineUnitProcessor.class)
+public class MultiCompanyBatchEngineUnitProcessor {
+
+	public void processBatchEngineUnits(Company company) {
+		for (Bundle bundle : _bundleBatchEngineUnits.keySet()) {
+			_processBatchEngineUnits(bundle, company);
+		}
+	}
+
+	public void registerBatchEngineUnits(
+		Bundle bundle, List<BatchEngineUnit> batchEngineUnits,
+		boolean process) {
+
+		_bundleBatchEngineUnits.put(bundle, batchEngineUnits);
+
+		if (process) {
+			_companyLocalService.forEachCompany(
+				company -> _processBatchEngineUnits(bundle, company));
+		}
+	}
+
+	public void unregister(Bundle bundle) {
+		_bundleProcessedCompanies.remove(bundle);
+		_bundleBatchEngineUnits.remove(bundle);
+	}
+
+	public void unregister(Company company) {
+		for (Set<Long> companyIds : _bundleProcessedCompanies.values()) {
+			companyIds.remove(company.getCompanyId());
+		}
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_bundleBatchEngineUnits.clear();
+		_bundleProcessedCompanies.clear();
+	}
+
+	private void _processBatchEngineUnits(Bundle bundle, Company company) {
+		Set<Long> companyIds = _bundleProcessedCompanies.computeIfAbsent(
+			bundle, key -> new HashSet<>());
+
+		if (companyIds.contains(company.getCompanyId())) {
+			return;
+		}
+
+		companyIds.add(company.getCompanyId());
+
+		_batchEngineUnitProcessor.processBatchEngineUnits(
+			TransformUtil.transform(
+				_bundleBatchEngineUnits.get(bundle),
+				batchEngineUnit -> new CompanyBatchEngineUnitWrapper(
+					batchEngineUnit, company)));
+	}
+
+	@Reference
+	private BatchEngineUnitProcessor _batchEngineUnitProcessor;
+
+	private final Map<Bundle, List<BatchEngineUnit>> _bundleBatchEngineUnits =
+		new HashMap<>();
+	private final Map<Bundle, Set<Long>> _bundleProcessedCompanies =
+		new HashMap<>();
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
+}
